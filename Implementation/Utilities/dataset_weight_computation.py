1c3464dbfc77fb44f858724ce8958bfb77ad6da5
guimcc <guim.casadella@gmail.com>
2024-05-09 19:26:19 +0200

    deleted cityscapes data

--- Implementation/Utilities/dataset_weight_computation.py
+++ Implementation/Utilities/dataset_weight_computation.py
@@ -2,37 +2,17 @@ import numpy as np
 
 # Class frequency of the dataset
 CLS_COUNTS = [
-    2036048947,
-    336030285,
-    1259773717,
-    36211195,
-    48487347,
-    67771817,
-    11510397,
-    30522367,
-    878732742,
-    63964778,
-    221459205,
-    67202385,
-    7444903,
-    386502898,
-    14775005,
-    12995799,
-    12863932,
-    5445909,
-    22849764,
+    '',
 ]
 # Class names of the dataset
 CLS_NAMES = [
-    "Road", "Sidewalk", "Building", "Wall", "Fence", "Pole", "Traffic Light", 
-    "Traffic Sign", "Vegetation", "Terrain", "Sky", "Person", "Rider", "Car", 
-    "Truck", "Bus", "Train", "Motorcycle", "Bicycle", "Void"
+    '',
 ]
 # Smoothing value
 CLS_SMOOTH = 1
 
 # Number of classes the dataset
-N = 19
+N = 
 
 cls_counts = np.array(CLS_COUNTS)
 total_counts = np.sum(cls_counts)
